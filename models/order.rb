2a41f5670c672748cfcf535aabfcd2df2f2d0443
Andy Kuan <andy952737@gmail.com>
2023-04-21 11:33:39 +0800

    model text record

--- models/order.rb
+++ models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+	#  id         :bigint           not null, primary key
+	#  slug       :string(255)
+	#  amount     :integer
+	#  name       :string(255)
+	#  email      :string(255)
+	#  paid       :boolean(255), default: false
+	#  token      :string(255)
+	#  price      :integer
+	#  created_at :datetime         not null
+	#  updated_at :datetime         not null
 end
